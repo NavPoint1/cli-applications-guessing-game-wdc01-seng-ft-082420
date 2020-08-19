66d9e186c37f0cf0ccf95433f06eae36c8ca79f5
outstanding-publisher-2341 <zaxmaf@gmail.com>
2020-08-19 08:12:20 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,10 +3,8 @@ require 'pry'
 
 def run_guessing_game
   magic_number = rand(6) + 1
-  binding.pry
   puts "Guess a number from 1 to 6"
   guess = gets.chomp
-  binding.pry
   if guess == magic_number
     puts "You guessed the correct number!"
   elsif guess == "exit"
