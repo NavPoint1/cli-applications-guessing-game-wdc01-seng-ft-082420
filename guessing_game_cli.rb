1dc0e097dc3b59bd18d5054b213c1abea24f6bfd
outstanding-publisher-2341 <zaxmaf@gmail.com>
2020-08-19 08:00:07 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,5 +2,13 @@
 
 def run_guessing_game
   magic_number = rand(6) + 1
-  puts ""
+  puts "Guess a number from 1 to 6"
+  guess = gets.chomp
+  if guess == magic_number
+    
+  elsif guess == "exit"
+    puts "Goodbye!"
+  else
+    
+  end
 end
